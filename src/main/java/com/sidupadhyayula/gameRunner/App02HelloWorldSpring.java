7976b5af6be2961c9c45b659275331dbd281ed44
Sai Siddharth Upadhyayula <usaisiddarth@gmail.com>
2024-01-04 23:30:44 -0500

    Created Spring Context and beans for  new app

--- src/main/java/com/sidupadhyayula/gameRunner/App02HelloWorldSpring.java
+++ src/main/java/com/sidupadhyayula/gameRunner/App02HelloWorldSpring.java
@@ -1,5 +1,7 @@
 package com.sidupadhyayula.gameRunner;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App02HelloWorldSpring {
@@ -8,6 +10,22 @@ public class App02HelloWorldSpring {
 		
 		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 		
+		System.out.println(context.getBean("name"));
+		
+		System.out.println(context.getBean("age"));
+		
+		System.out.println(context.getBean("person"));
+		
+		System.out.println(context.getBean("address2"));
+		
+		System.out.println(context.getBean("person2MethodCall"));
+		
+		System.out.println(context.getBean("person3Parameters"));
+		
+		Arrays.stream(context.getBeanDefinitionNames())
+    		.forEach(System.out::println);
+
+		
 	}
- 
+	
 }
--- src/main/java/com/sidupadhyayula/gameRunner/HelloWorldConfiguration.java
+++ src/main/java/com/sidupadhyayula/gameRunner/HelloWorldConfiguration.java
@@ -1,8 +1,43 @@
 package com.sidupadhyayula.gameRunner;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+record Person (String name, int age, Address address) { };
+
+record Address(String firstLine, String city){ };
 
 @Configuration 
 public class HelloWorldConfiguration {
+	
+	@Bean
+	public String name() {
+		return "Sid";
+	} 
+	
+	@Bean
+	public int age() {
+		return 24;
+	} 
+	
+	@Bean
+	public Person person() {
+		return new Person("Ravi", 20, new Address("Main Street", "Utrecht"));		
+	}
+	
+	@Bean
+	public Person person2MethodCall() {
+		return new Person(name(), age(), address()); //creating this bean in case we need to use existing information to use in the new bean 		
+	}
 
+	@Bean
+	public Person person3Parameters(String name, int age, Address address3) {
+		//this bean is created in case we need to use existing information by using parameters instead of methods 
+		return new Person(name, age, address3); //name, age		
+	}
+	
+	@Bean(name = "address2") 
+	public Address address() {
+		return new Address("Mainstreet", "Jacksonville");
+	}
 }
